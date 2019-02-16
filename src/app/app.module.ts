ee5573f3c7a6dd3b91325e7bd3fc596e02400b68
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2019-02-16 00:53:24 -0200

    Criando as primeiras rotas - secao5-aula36

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
+import {ROUTES} from './app.routes'
+
+
+
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -20,7 +24,7 @@ import { AboutComponent } from './about/about.component'
   imports: [
     BrowserModule,
     HttpModule,
-    RouterModule
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
