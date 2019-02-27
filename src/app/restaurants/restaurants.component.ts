20555d88e1517cf1e35f8c95b31e6319fbd32a47
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2019-02-26 23:55:17 -0300

    criacao do api json e servico http restaurants

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -17,7 +17,8 @@ export class RestaurantsComponent implements OnInit {
 
   ngOnInit() {
 
-    this.restaurants = this.restaurantsService.restaurants()
+     this.restaurantsService.restaurants()
+     .subscribe(restaurants => this.restaurants = restaurants)
 
   }
 
--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -1,35 +1,23 @@
+import { Injectable } from "@angular/core";
+import {Http} from '@angular/http'
+
 import { Restaurant } from "./restaurant/restaurant.model";
-
-export class RestaurantsService {
-
-    rests: Restaurant[] = [
-
-        {
-            id: "bread-bakery",
-            name: "Bread & Bakery",
-            category: "Bakery",
-            deliveryEstimate: "25m",
-            rating: 4.9,
-            imagePath: "assets/img/restaurants/breadbakery.png"
-
-        },
-        {
-            id: "burger-house",
-            name: "Burger House",
-            category: "Hamburgers",
-            deliveryEstimate: "100m",
-            rating: 3.5,
-            imagePath: "assets/img/restaurants/burgerhouse.png"
-
-        }
-    ]
+import {MEAT_API} from '../app.api'
+import { Observable } from "rxjs/Observable";
+import 'rxjs/add/operator/map'
 
 
+@Injectable()
+export class RestaurantsService {   
 
-    constructor() { }
 
-    restaurants(): Restaurant[] {
-        return this.rests;
+    constructor(private http: Http) { }
+
+    restaurants(): Observable<Restaurant[]> {
+
+        return this.http.get(`${MEAT_API}/restaurants`)
+        .map(response => response.json())
+        
     }
 
 
