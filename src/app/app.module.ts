5ba339ae17b07fd85de998a8ef1d2d737e393587
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2019-02-28 00:22:15 -0300

    detalhes do restaurante - parte 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,10 @@ import { AboutComponent } from './about/about.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantComponent } from './restaurants/restaurant/restaurant.component'
 import { RestaurantsService } from './restaurants/restaurants.service';
+import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
+import { MenuComponent } from './restaurant-detail/src/app/restaurant-detail/menu/menu.component';
+import { ShoppingCartComponent } from './restaurant-detail/src/app/restaurant-detail/shopping-cart/shopping-cart.component';
+import { MenuItemComponent } from './restaurant-detail/src/app/restaurant-detail/menu-item/menu-item.component';
 
 
 @NgModule({
@@ -24,7 +28,11 @@ import { RestaurantsService } from './restaurants/restaurants.service';
     HomeComponent,
     AboutComponent,
     RestaurantsComponent,
-    RestaurantComponent
+    RestaurantComponent,
+    RestaurantDetailComponent,
+    MenuComponent,
+    ShoppingCartComponent,
+    MenuItemComponent
   ],
   imports: [
     BrowserModule,
